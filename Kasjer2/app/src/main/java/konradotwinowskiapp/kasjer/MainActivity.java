379d2b70c48613b0301465d8519c28bcb07390f7
Memotoshi <konrad.otwinowski@gmail.com>
2017-05-23 14:59:27 +0200

    Kasjer

---
+++ Kasjer2/app/src/main/java/konradotwinowskiapp/kasjer/MainActivity.java
@@ -0,0 +1,39 @@
+package konradotwinowskiapp.kasjer;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.settings_id) {
+
+            startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
