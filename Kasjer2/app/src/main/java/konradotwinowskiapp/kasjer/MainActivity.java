ebd953e5f26aeed11fc4690871f9365a7bf494d1
Memotoshi <konrad.otwinowski@gmail.com>
2017-05-24 20:28:56 +0200

    Settings and MainActivity done.

--- Kasjer2/app/src/main/java/konradotwinowskiapp/kasjer/MainActivity.java
+++ Kasjer2/app/src/main/java/konradotwinowskiapp/kasjer/MainActivity.java
@@ -6,12 +6,28 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName();
+    private Button LoginButton;
+    private RequestQueue mRequestQueue;
+    private StringRequest stringRequest;
+    private SharedPreferences sharedPref;
+    EditText KasaEditText;
 
 
     @Override
@@ -38,17 +54,49 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button LoginButton = (Button) findViewById(R.id.LoginButton);
+        KasaEditText = (EditText) findViewById(R.id.KasaEditText);
+        LoginButton = (Button) findViewById(R.id.LoginButton);
 
-        SharedPreferences sharedPref = getSharedPreferences("IPInfo", Context.MODE_PRIVATE);
+
+        this.sharedPref = getSharedPreferences("IPInfo", Context.MODE_PRIVATE);
 
         LoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                startActivity(new Intent(MainActivity.this, SecondActivity.class));
+                sendRequestAndPrintResponse();
             }
         });
+    }
+
+    private void sendRequestAndPrintResponse() {
+
+        final String targetName = KasaEditText.getText().toString();
+        mRequestQueue = Volley.newRequestQueue(this);
+
+        String IP = this.sharedPref.getString("IP", "");
+
+        stringRequest = new StringRequest(Request.Method.GET, "http://" + IP + "/open/" + targetName, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                Log.i(TAG, "Response : " + response.toString());
+                final Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                intent.putExtra("Kasa", targetName);
+                //startActivity(new Intent(MainActivity.this, SecondActivity.class));
+                startActivity(intent);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i(TAG, "Error : " + error.toString());
+            }
+        });
+
+        mRequestQueue.add(stringRequest);
+
+
+
     }
 
 
--- Kasjer2/app/src/main/java/konradotwinowskiapp/kasjer/SecondActivity.java
+++ Kasjer2/app/src/main/java/konradotwinowskiapp/kasjer/SecondActivity.java
@@ -2,12 +2,20 @@ package konradotwinowskiapp.kasjer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    TextView KasaTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        KasaTextView = (TextView) findViewById(R.id.KasaTextView);
+        KasaTextView.setText(getIntent().getStringExtra("Kasa") + " kurwa" );
+
     }
 }
